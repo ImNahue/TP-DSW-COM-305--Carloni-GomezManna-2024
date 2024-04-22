5fddacaab969066c7d9be0b2de7b936c0af7ac87
ImNahue <nahuelcarloni25@gmail.com>
2024-04-22 16:16:13 -0300

    Algunos cambios

--- Login/index.js
+++ Login/index.js
@@ -20,8 +20,8 @@ app.use(cookieParser())
 
 
 //Rutas
-app.get("/",authorization.soloPublico, (req,res)=> res.sendFile(__dirname + "/pages/login.html"));
-app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "/pages/register.html"));
-app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/admin.html"));
+app.get("/",authorization.soloPublico, (req,res)=> res.sendFile(__dirname + "./pages/login.js"));
+app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "./pages/register.html"));
+app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "./pages/admin/admin.html"));
 app.post("/api/login",authentication.login);
 app.post("/api/register",authentication.register);
